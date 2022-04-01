22c8682ab4a42e9ef67e7120cd312aa4fd8c9ca7
Lnicolus <lnicolus86@gmail.com>
2022-03-31 21:48:47 -0300

    entrega 5

--- server.js
+++ server.js
@@ -1,40 +1,21 @@
-const Contenedor = require("./contenedor");
-const express = require("express");
-const app = express();
+import express from 'express';
+import multer from 'multer';
+import router from './routes/routes.js';
 
-let productos = new Contenedor("./productos.txt");
+export const app = express();
 
-app.get('/productos', (req, res) => {
-  (async () => {
-    await productos.getAll().then((resolve) => {
-      res.send(resolve);
-    });
-  })();
-});
+app.use(express.static('views'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', router);
 
 app.get('/', (req, res) => {
-  (async () => {
-    await productos.getAll().then((resolve) => {
-      res.send(resolve);
-    });
-  })();
+    res.sendFile(__dirname + '/views/index.html')
 });
 
-app.get('/productoRandom', (req, res) => {
-  (async () => {
-    let lista = await productos.getAll().then((res) => res);
-    if(lista.length === 0) {
-      res.status().send('No hay productos');
-    }
-    else {
-      res.send(lista[Math.floor(Math.random() * lista.length)]);
-    }
-  })();
-});
-
-let PORT = 8080;
+const PORT = 8080;
 
 const server = app.listen(PORT, () => {
-  console.log(`Servidor Express escuchando peticiones en el puerto ${server.address().port}`);
+    console.log(`Server is running on port: ${server.address().port}`);
 });
-server.on("error", (error) => console.log(`Error en servidor ${error}`));
+server.on('error', error => console.log(`error running server: ${error}`));
